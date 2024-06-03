be300d8c6bc508362a8409edcc085f5cd35696e1
Sanbedan <sanbedan@debox.co.in>
2024-06-03 12:18:35 +0530

    Little change in Spotlight in Hero

--- components/Hero/Hero4.tsx
+++ components/Hero/Hero4.tsx
@@ -14,10 +14,7 @@ const Heroopt3 = () => {
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
           fill="white"
         />
-        <Spotlight
-          className="h-[80vh] w-[50vw] top-10 left-full"
-          fill="purple"
-        />
+        <Spotlight className="h-[80vh] w-[50vw] top-10 left-full" fill="blue" />
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
