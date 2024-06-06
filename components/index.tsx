25a00bdbe57670b4a18bdd84c737a4eea00fd070
Sanbedan <sanbedan@debox.co.in>
2024-06-06 20:44:17 +0530

    Mods done

--- components/index.tsx
+++ components/index.tsx
@@ -1,6 +1,5 @@
 export { default as Navbar } from "@/components/Navbar/navbar";
 export { default as Header } from "@/components/Hero/Hero2";
-export { default as FeatureSection } from "@/components/Features/FeatureSection";
 export { default as FAQnew } from "@/components/Faq/new";
 export { default as SecFooter } from "@/components/SecFooter/index";
 export { default as Footertwo } from "@/components/Footer/Footer";
