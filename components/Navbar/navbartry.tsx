5d197b92c0d3782190bffa9ecb3829afd42a1141
Sanbedan <sanbedan@debox.co.in>
2024-06-10 20:25:44 +0530

    Fixes done

--- components/Navbar/navbartry.tsx
+++ components/Navbar/navbartry.tsx
@@ -111,7 +111,7 @@ const Header: React.FC = () => {
                               close();
                             }}
                           >
-                            <Modalbtn text={"Join Us"} emailp="" />
+                            <Modalbtn text={"Join Us"} />
                           </div>
                         </Popover.Panel>
                       </>
